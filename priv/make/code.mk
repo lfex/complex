6467ae14e9362bb284c5aa078b41f3ba579cbfc2
Duncan McGreggor <oubiwann@gmail.com>
2016-10-28 11:57:37 -0500

    Added new make targets.

--- priv/make/code.mk
+++ priv/make/code.mk
@@ -2,7 +2,7 @@ compile:
 	rebar3 compile
 
 check:
-	@rebar3 as test eunit
+	rebar3 as test lfe test -t unit
 
 repl:
 	@rebar3 as dev compile
@@ -13,7 +13,31 @@ shell:
 
 clean:
 	@rebar3 clean
-	@rm -rf ebin/* _build/*/lib/$(PROJECT)
+	@rm -rf ebin/* _build/*/lib/$(PROJECT) rebar.lock
 
 clean-all: clean
 	@rebar3 as dev lfe clean
+
+push:
+	git push github master
+	git push gitlab master
+
+push-tags:
+	git push github --tags
+	git push gitlab --tags
+
+push-all: push push-tags
+
+build-github: clean
+	rebar3 compile
+
+build-gitlab: clean
+	rebar3 as gitlab compile
+
+build-hexpm: clean
+	rebar3 as hexpm compile
+
+build-all: build-github build-gitlab build-hexpm
+
+publish: clean
+	rebar3 as hexpm hex publish
